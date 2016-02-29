1a9a20a25cacee8469fb8c624e5c6ac89a3fb66b
irfan87 <nerve2009@yahoo.com>
2016-03-01 02:05:08 +0800

    add delete

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -36,6 +36,10 @@ class CoffeeShopsController < ApplicationController
   end
 
   def destroy
+    @coffee_shop = CoffeeShop.find(params[:id])
+    @coffee_shop.destroy
+
+    redirect_to coffee_shops_path
   end
 
   private
